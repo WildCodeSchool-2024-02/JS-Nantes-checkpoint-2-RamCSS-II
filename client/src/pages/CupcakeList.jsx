df694b2386121e64366d973938eb8e1f4c44ec91
ATN <contact.atn35@gmail.com>
2024-05-14 12:46:19 +0200

    Je me sens perdu dans un labyrinthe d'ignorance, atteint par l'oublie presque sans la moindre lueur d'espoir à l'horizon mis a par chatGPT qui ce fait de plus en plus indispensable.

--- client/src/pages/CupcakeList.jsx
+++ client/src/pages/CupcakeList.jsx
@@ -1,73 +1,94 @@
-import { useLoaderData } from "react-router-dom";
-import Cupcake from "../components/Cupcake";
+// import { useLoaderData } from "react-router-dom";
+// import Cupcake from "../components/Cupcake";
+// import { useEffect, useState } from "react";
 
-/* ************************************************************************* */
-const someCupcakes = [];
-someCupcakes.push(
-  {
-    id: 10,
-    accessory_id: "4",
-    accessory: "wcs",
-    color1: "blue",
-    color2: "white",
-    color3: "red",
-    name: "France",
-  },
-  {
-    id: 11,
-    accessory_id: "4",
-    accessory: "wcs",
-    color1: "yellow",
-    color2: "red",
-    color3: "black",
-    name: "Germany",
-  },
-  {
-    id: 27,
-    accessory_id: "5",
-    accessory: "christmas-candy",
-    color1: "yellow",
-    color2: "blue",
-    color3: "blue",
-    name: "Sweden",
-  }
-);
+// /* ************************************************************************* */
+// const someCupcakes = [];
+// someCupcakes.push(
+//   {
+//     id: 10,
+//     accessory_id: "4",
+//     accessory: "wcs",
+//     color1: "blue",
+//     color2: "white",
+//     color3: "red",
+//     name: "France",
+//   },
+//   {
+//     id: 11,
+//     accessory_id: "4",
+//     accessory: "wcs",
+//     color1: "yellow",
+//     color2: "red",
+//     color3: "black",
+//     name: "Germany",
+//   },
+//   {
+//     id: 27,
+//     accessory_id: "5",
+//     accessory: "christmas-candy",
+//     color1: "yellow",
+//     color2: "blue",
+//     color3: "blue",
+//     name: "Sweden",
+//   }
+// );
 
-/* you can use someCupcakes if you're stucked on step 1 */
-/* if you're fine with step 1, just ignore this ;) */
-/* ************************************************************************* */
+// /* you can use someCupcakes if you're stucked on step 1 */
+// /* if you're fine with step 1, just ignore this ;) */
+// /* ************************************************************************* */
 
-function CupcakeList() {
-  // Step 1: get all cupcakes
-  console.info(useLoaderData());
+// function CupcakeList() {
+//   const [accessories, setAccessories] = useState([]);
+//   const [selectedAccessory, setSelectedAccessory] = useState("");
+//   // const [cupcakes, setCupcakes] = useState([]);
 
-  // Step 3: get all accessories
+//   useEffect(() => {
+//     fetch("http://localhost:3310/api/cupcakes")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setAccessories(data);
+//         console.info("Accessories Data:", data);
+//       })
+//       .catch((error) =>
+//         console.error("Erreur lors de la récupération des accessoires:", error)
+//       );
 
-  // Step 5: create filter state
+//   }, []);
 
-  return (
-    <>
-      <h1>My cupcakes</h1>
-      <form className="center">
-        <label htmlFor="cupcake-select">
-          {/* Step 5: use a controlled component for select */}
-          Filter by{" "}
-          <select id="cupcake-select">
-            <option value="">---</option>
-            {/* Step 4: add an option for each accessory */}
-          </select>
-        </label>
-      </form>
-      <ul className="cupcake-list" id="cupcake-list">
-        {/* Step 2: repeat this block for each cupcake */}
-        {/* Step 5: filter cupcakes before repeating */}
-        <li className="cupcake-item">
-          <Cupcake />
-        </li>
-        {/* end of block */}
-      </ul>
-    </>
-  );
-}
+//   const filteredCupcakes = selectedAccessory
+//     ? cupcakes.filter((cupcake) => cupcake.accessory_id === selectedAccessory)
+//     : cupcakes;
 
-export default CupcakeList;
+//   return (
+//     <>
+//       <h1>My cupcakes</h1>
+//       <form className="center">
+//         <label htmlFor="cupcake-select">
+//           Filter by{" "}
+//           <select
+//             id="cupcake-select"
+//             value={selectedAccessory}
+//             onChange={(e) => setSelectedAccessory(e.target.value)}
+//           >
+//             <option value="">---</option>
+//             {accessories.map((accessory) => (
+//               <option key={accessory.id} value={accessory.id}>
+//                 {accessory.name}
+//               </option>
+//             ))}
+//           </select>
+//         </label>
+//       </form>
+//       <ul className="cupcake-list" id="cupcake-list">
+//         {filteredCupcakes.map((cupcake) => (
+//           <li key={cupcake.id} className="cupcake-item">
+//             <Cupcake cupcake={cupcake} />
+//           </li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// }
+
+// export default CupcakeList;
