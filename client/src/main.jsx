fd02bd078452064cb2b9c3b111243d5e4136cc45
hakim-azizi <160917355+hakim-azizi@users.noreply.github.com>
2024-05-14 09:48:30 +0200

    fin de l'étape 1 fetch de l'étape 1 dans le loader

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -26,6 +26,9 @@ const router = createBrowserRouter([
         path: "/cupcakes",
         element: <CupcakeList />,
         // Step 1: load data here
+        loader: () =>
+          fetch(" http://localhost:3310/api/cupcakes ")
+            .then((response) => response.json())
       },
     ],
   },
